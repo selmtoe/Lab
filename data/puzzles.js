e0831d6ab1a5426155f6cadd50c0112bd0019a3d
Intai <jhajan634@gmail.com>
2026-01-02 13:26:35 +0900

    Add initial puzzles data list to puzzles.js

---
+++ data/puzzles.js
@@ -0,0 +1,20 @@
+// 謎テト（Puzzle）データリスト
+window.puzzlesData = [
+    {
+        id: 1,
+        date: '2025-12-10',
+        title: 'DT砲からの派生練習',
+        stars: 4, // 1〜10
+        description: 'TSTに行かずに妥協するルートの検証。',
+        link: 'https://selmtoe.github.io/Tetris_Simulator/?data=...' // ここにシミュレータの長いURLを貼る
+    },
+    {
+        id: 2,
+        date: '2025-12-12',
+        title: '超難問：S/Z軸のリカバリー',
+        stars: 8,
+        description: '地形が悪くなった状態からの復帰手順。',
+        link: '#'
+    }
+    // 新しいデータをここに追加...
+];
